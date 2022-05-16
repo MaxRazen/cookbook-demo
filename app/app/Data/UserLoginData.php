02b98e5fc90d43067d4060a5ba19fc860efcd76a
Maxim Razenkov <maxwell.devmail@gmail.com>
2022-05-16 11:00:58 +0300

    split login logic from request

---
+++ app/app/Data/UserLoginData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class UserLoginData extends DataTransferObject
+{
+    public string $email;
+    public string $password;
+    public bool $remember;
+    public string $ip;
+}
--- app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,47 +3,32 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\UserLoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Services\UserAuthService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create()
+    public function create(): View
     {
         return view('auth.login');
     }
 
-    /**
-     * Handle an incoming authentication request.
-     *
-     * @param  \App\Http\Requests\Auth\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(LoginRequest $request)
+    public function store(UserLoginRequest $request, UserAuthService $authService): RedirectResponse
     {
-        $request->authenticate();
+        $authService->login($request->data());
 
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
 
-    /**
-     * Destroy an authenticated session.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(Request $request)
+    public function destroy(Request $request, UserAuthService $authService): RedirectResponse
     {
-        Auth::guard('web')->logout();
+        $authService->logout();
 
         $request->session()->invalidate();
 
